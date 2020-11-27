4d9d68301d97e1a516d4aa3aa97b61e7be3092be
Drake Tyksinski <drake.tyksinski@live.com>
2020-11-26 20:28:47 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("linkedIn").onclick = click1
+
+function click1() {
+    alert("Follow me on linked in! https://www.linkedin.com/in/draketyksinski")
+}
+
+document.getElementById("twitch").onclick = click2
+
+function click2() {
+    alert("Follow me on twitch! https://www.twitch.tv/Yawprs")
+}
+
+document.getElementById("instagram").onclick = click3
+
+function click3() {
+    alert("Follow me on Instagram! https://www.instagram.com/drakegnarly/")
+}
